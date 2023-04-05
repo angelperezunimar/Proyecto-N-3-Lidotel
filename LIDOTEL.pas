812d7d79d3f01094481cbb9382a4f924da177296
Samuel <smarcano.8764@unimar.edu.ve>
2023-04-04 23:16:14 -0400

    Se agregan variables para empezar la estructura del sistema

--- LIDOTEL.pas
+++ LIDOTEL.pas
@@ -1,10 +1,13 @@
-program lidotel;
 
+{Sistema de reservacion para el hotel LIDOTEL; realizado por Samuel Marcano, Angel Perez, Abdl Taktak}
+
+program Sistema_de_Reservacionlidotel;
 uses crt;
-var i : byte;
+{Se empiezan inicializando variables necesarias para el programa}
+var  Menu, Datos: string;
+var x1, cod1: integer;
 
 BEGIN
-	
-	
+
 END.
 
